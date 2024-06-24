a24ef274286a045fb5ac55a51a471ed32dc7ace9
nata13pr <Nata13pr@gmail.com>
2024-06-24 21:01:10 +0300

    feat:added TVShowPage.tsx

--- src/pages/card/CardPage.tsx
+++ src/pages/card/CardPage.tsx
@@ -3,7 +3,7 @@ import {useAppDispatch} from "../../hooks/reduxHooks";
 import {movieActions} from "../../redux/slices/movieSlice";
 import {useParams,} from "react-router-dom";
 import {movieService} from "../../services/movie/movieService";
-import MoviesListCardComponent from "../../components/MovieListCard/MoviesListCardComponent";
+import MoviesListCardComponent from "../../components/movieListCard/MoviesListCardComponent";
 import StarsRatingComponent from "../../components/starsRating/StarsRatingComponent";
 
 const CardPage = () => {
